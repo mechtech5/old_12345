16892eb99a8498ad81cbd91644829d0db7075cf6
Ayush Likhar <alikhar@laxyo.in>
2019-09-13 18:19:10 +0530

    added new tables

---
+++ app/Http/Controllers/EmployersController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employers;
+use Illuminate\Http\Request;
+
+class EmployersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Employers  $employers
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Employers $employers)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Employers  $employers
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Employers $employers)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Employers  $employers
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Employers $employers)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Employers  $employers
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Employers $employers)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/JobalertController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jobalert;
+use Illuminate\Http\Request;
+
+class JobalertController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Jobalert  $jobalert
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Jobalert $jobalert)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Jobalert  $jobalert
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Jobalert $jobalert)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Jobalert  $jobalert
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Jobalert $jobalert)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Jobalert  $jobalert
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Jobalert $jobalert)
+    {
+        //
+    }
+}
