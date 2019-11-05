61248a77519bf8f3c37eb44f59fbe7db32a2e8d1
Ayush Likhar <ayush.likhar@outlook.com>
2019-11-05 09:01:17 +0530

    added comments component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,24 +2,25 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-Vue.component(
-    'passport-clients',
-    require('./components/passport/Clients.vue').default
-);
+// Vue.component(
+//     'passport-clients',
+//     require('./components/passport/Clients.vue').default
+// );
 
-Vue.component(
-    'passport-authorized-clients',
-    require('./components/passport/AuthorizedClients.vue').default
-);
+// Vue.component(
+//     'passport-authorized-clients',
+//     require('./components/passport/AuthorizedClients.vue').default
+// );
 
-Vue.component(
-    'passport-personal-access-tokens',
-    require('./components/passport/PersonalAccessTokens.vue').default
-);
+// Vue.component(
+//     'passport-personal-access-tokens',
+//     require('./components/passport/PersonalAccessTokens.vue').default
+// );
 
-Vue.component('feed-component', require('./components/Feed.vue').default);
-Vue.component('home-component', require('./components/Home.vue').default);
-Vue.component('profile-component', require('./components/Profile.vue').default);
+Vue.component('feed-component', require('./components/feed-component.vue').default)
+Vue.component('home-component', require('./components/home-component.vue').default)
+Vue.component('profile-component', require('./components/profile-component.vue').default)
+Vue.component('comments-component', require('./components/comments-component.vue').default)
 
 
 
--- resources/views/ayushiblogs/show.blade.php
+++ resources/views/ayushiblogs/show.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <title></title>
-    
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     @include('partials._wordify.head')
     <style>
       .embedded_image img{
@@ -13,231 +13,120 @@
   </head>
 
   <body>
-    <div class="wrap">
+    <div class="wrap" id="app">
 
-    @include('partials._wordify.nav')
+      @include('partials._wordify.nav')
 
-    <section class="site-section py-lg">
-      <div class="container">
-        
-        <div class="row blog-entries element-animate">
+      <section class="site-section py-lg">
+        <div class="container">
+          
+          <div class="row blog-entries element-animate">
 
-          <div class="col-md-12 col-lg-8 main-content">
-            <img src="{{ $post->featured_image }}" alt="Image" class="img-fluid mb-5">
-             <div class="post-meta">
-              <span class="author mr-2"><img src="{{ $author->avatar }}" alt="Colorlib" class="mr-2"> {{ $author->name }}</span>&bullet;
-              <span class="mr-2">{{$post->publish_date->format('d F Y')}} </span> 
-              {{-- &bullet;
-              <span class="ml-2"><span class="fa fa-comments"></span> 3</span> --}}
-            </div>
-            <h1 class="mb-4">{{ $post->title }}</h1>
-            @foreach($post->tags as $tag)
-              <a class="category mb-5" href="#">{{ $tag->name }}</a> 
-            @endforeach
-
-            <div class="post-content-body">
-              {!! $post->body !!}
-            </div>
-
-            
-            {{-- <div class="pt-5">
-              <p>Categories:  <a href="#">Food</a>, <a href="#">Travel</a>  Tags: <a href="#">#manila</a>, <a href="#">#asia</a></p>
-            </div> --}}
-
-
-            {{-- <div class="pt-5">
-              <h3 class="mb-5">6 Comments</h3>
-              <ul class="comment-list">
-                <li class="comment">
-                  <div class="vcard">
-                    <img src="images/person_1.jpg" alt="Image placeholder">
-                  </div>
-                  <div class="comment-body">
-                    <h3>Jean Doe</h3>
-                    <div class="meta">January 9, 2018 at 2:21pm</div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-                    <p><a href="#" class="reply rounded">Reply</a></p>
-                  </div>
-                </li>
-
-                <li class="comment">
-                  <div class="vcard">
-                    <img src="images/person_1.jpg" alt="Image placeholder">
-                  </div>
-                  <div class="comment-body">
-                    <h3>Jean Doe</h3>
-                    <div class="meta">January 9, 2018 at 2:21pm</div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-                    <p><a href="#" class="reply rounded">Reply</a></p>
-                  </div>
-
-                  <ul class="children">
-                    <li class="comment">
-                      <div class="vcard">
-                        <img src="images/person_1.jpg" alt="Image placeholder">
-                      </div>
-                      <div class="comment-body">
-                        <h3>Jean Doe</h3>
-                        <div class="meta">January 9, 2018 at 2:21pm</div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-                        <p><a href="#" class="reply rounded">Reply</a></p>
-                      </div>
-
-
-                      <ul class="children">
-                        <li class="comment">
-                          <div class="vcard">
-                            <img src="images/person_1.jpg" alt="Image placeholder">
-                          </div>
-                          <div class="comment-body">
-                            <h3>Jean Doe</h3>
-                            <div class="meta">January 9, 2018 at 2:21pm</div>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-                            <p><a href="#" class="reply rounded">Reply</a></p>
-                          </div>
-
-                            <ul class="children">
-                              <li class="comment">
-                                <div class="vcard">
-                                  <img src="images/person_1.jpg" alt="Image placeholder">
-                                </div>
-                                <div class="comment-body">
-                                  <h3>Jean Doe</h3>
-                                  <div class="meta">January 9, 2018 at 2:21pm</div>
-                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-                                  <p><a href="#" class="reply rounded">Reply</a></p>
-                                </div>
-                              </li>
-                            </ul>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-
-                <li class="comment">
-                  <div class="vcard">
-                    <img src="images/person_1.jpg" alt="Image placeholder">
-                  </div>
-                  <div class="comment-body">
-                    <h3>Jean Doe</h3>
-                    <div class="meta">January 9, 2018 at 2:21pm</div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-                    <p><a href="#" class="reply rounded">Reply</a></p>
-                  </div>
-                </li>
-              </ul>
-              <!-- END comment-list -->
-              
-              <div class="comment-form-wrap pt-5">
-                <h3 class="mb-5">Leave a comment</h3>
-                <form action="#" class="p-5 bg-light">
-                  <div class="form-group">
-                    <label for="name">Name *</label>
-                    <input type="text" class="form-control" id="name">
-                  </div>
-                  <div class="form-group">
-                    <label for="email">Email *</label>
-                    <input type="email" class="form-control" id="email">
-                  </div>
-                  <div class="form-group">
-                    <label for="website">Website</label>
-                    <input type="url" class="form-control" id="website">
-                  </div>
-
-                  <div class="form-group">
-                    <label for="message">Message</label>
-                    <textarea name="" id="message" cols="30" rows="10" class="form-control"></textarea>
-                  </div>
-                  <div class="form-group">
-                    <input type="submit" value="Post Comment" class="btn btn-primary">
-                  </div>
-
-                </form>
+            <div class="col-md-12 col-lg-8 main-content">
+              <img src="{{ $post->featured_image }}" alt="Image" class="img-fluid mb-5">
+               <div class="post-meta">
+                <span class="author mr-2"><img src="{{ $author->avatar }}" alt="Colorlib" class="mr-2"> {{ $author->name }}</span>&bullet;
+                <span class="mr-2">{{$post->publish_date->format('d F Y')}} </span> 
+                {{-- &bullet;
+                <span class="ml-2"><span class="fa fa-comments"></span> 3</span> --}}
               </div>
-            </div> --}}
+              <h1 class="mb-4">{{ $post->title }}</h1>
+              @foreach($post->tags as $tag)
+                <a class="category mb-5" href="#">{{ $tag->name }}</a> 
+              @endforeach
 
-          </div>
+              <div class="post-content-body">
+                {!! $post->body !!}
+              </div>
 
-          <!-- END main-content -->
-
-          <div class="col-md-12 col-lg-4 sidebar">
-              {{-- <div class="sidebar-box search-form-wrap">
-                <form action="#" class="search-form">
-                  <div class="form-group">
-                    <span class="icon fa fa-search"></span>
-                    <input type="text" class="form-control" id="s" placeholder="Type a keyword and hit enter">
-                  </div>
-                </form>
+              
+              {{-- <div class="pt-5">
+                <p>Categories:  <a href="#">Food</a>, <a href="#">Travel</a>  Tags: <a href="#">#manila</a>, <a href="#">#asia</a></p>
               </div> --}}
-              <!-- END sidebar-box -->
-              <div class="sidebar-box">
-                <div class="bio text-center">
-                  <img src="{{ $author->avatar }}" alt="Image Placeholder" class="img-fluid">
-                  <div class="bio-body">
-                    <h2>{{ $author->name }}</h2>
-                    <p>{!! $author->bio !!}</p>
-                    <p><a href="#" class="btn btn-primary btn-sm rounded">Read my bio</a></p>
-                    <p class="social">
-                      <a href="#" class="p-2"><span class="fa fa-facebook"></span></a>
-                      <a href="#" class="p-2"><span class="fa fa-twitter"></span></a>
-                      <a href="#" class="p-2"><span class="fa fa-instagram"></span></a>
-                      <a href="#" class="p-2"><span class="fa fa-youtube-play"></span></a>
-                    </p>
+
+              <comments-component></comments-component>
+
+            </div>
+
+            <!-- END main-content -->
+
+            <div class="col-md-12 col-lg-4 sidebar">
+                {{-- <div class="sidebar-box search-form-wrap">
+                  <form action="#" class="search-form">
+                    <div class="form-group">
+                      <span class="icon fa fa-search"></span>
+                      <input type="text" class="form-control" id="s" placeholder="Type a keyword and hit enter">
+                    </div>
+                  </form>
+                </div> --}}
+                <!-- END sidebar-box -->
+                <div class="sidebar-box">
+                  <div class="bio text-center">
+                    <img src="{{ $author->avatar }}" alt="Image Placeholder" class="img-fluid">
+                    <div class="bio-body">
+                      <h2>{{ $author->name }}</h2>
+                      <p>{!! $author->bio !!}</p>
+                      <p><a href="#" class="btn btn-primary btn-sm rounded">Read my bio</a></p>
+                      <p class="social">
+                        <a href="#" class="p-2"><span class="fa fa-facebook"></span></a>
+                        <a href="#" class="p-2"><span class="fa fa-twitter"></span></a>
+                        <a href="#" class="p-2"><span class="fa fa-instagram"></span></a>
+                        <a href="#" class="p-2"><span class="fa fa-youtube-play"></span></a>
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <!-- END sidebar-box -->  
-              {{-- <div class="sidebar-box">
-                <h3 class="heading">Popular Posts</h3>
-                <div class="post-entry-sidebar">
-                  <ul>
-                    @foreach($random_posts as $post)
-                    <li>
-                      <a href="">
-                        <img src="{{ $post->featured_image }}" alt="Image placeholder" class="mr-4">
-                        <div class="text">
-                          <h4>{{ $post->title }}</h4>
-                          <div class="post-meta">
-                            <span class="mr-2">{{$post->publish_date->format('F d, Y')}} </span>
+                <!-- END sidebar-box -->  
+                {{-- <div class="sidebar-box">
+                  <h3 class="heading">Popular Posts</h3>
+                  <div class="post-entry-sidebar">
+                    <ul>
+                      @foreach($random_posts as $post)
+                      <li>
+                        <a href="">
+                          <img src="{{ $post->featured_image }}" alt="Image placeholder" class="mr-4">
+                          <div class="text">
+                            <h4>{{ $post->title }}</h4>
+                            <div class="post-meta">
+                              <span class="mr-2">{{$post->publish_date->format('F d, Y')}} </span>
+                            </div>
                           </div>
-                        </div>
-                      </a>
-                    </li>
+                        </a>
+                      </li>
+                      @endforeach
+                    </ul>
+                  </div>
+                </div> --}}
+                <!-- END sidebar-box -->
+
+                {{-- <div class="sidebar-box">
+                  <h3 class="heading">Categories</h3>
+                  <ul class="categories">
+                    <li><a href="#">Food <span>(12)</span></a></li>
+                    <li><a href="#">Travel <span>(22)</span></a></li>
+                    <li><a href="#">Lifestyle <span>(37)</span></a></li>
+                    <li><a href="#">Business <span>(42)</span></a></li>
+                    <li><a href="#">Adventure <span>(14)</span></a></li>
+                  </ul>
+                </div> --}}
+                <!-- END sidebar-box -->
+
+                <div class="sidebar-box">
+                  <h3 class="heading">Tags</h3>
+                  <ul class="tags">
+                    @foreach($tags as $tag)
+                      <li><a href="#">{{ $tag->name }}</a></li>
                     @endforeach
                   </ul>
                 </div>
-              </div> --}}
-              <!-- END sidebar-box -->
-
-              {{-- <div class="sidebar-box">
-                <h3 class="heading">Categories</h3>
-                <ul class="categories">
-                  <li><a href="#">Food <span>(12)</span></a></li>
-                  <li><a href="#">Travel <span>(22)</span></a></li>
-                  <li><a href="#">Lifestyle <span>(37)</span></a></li>
-                  <li><a href="#">Business <span>(42)</span></a></li>
-                  <li><a href="#">Adventure <span>(14)</span></a></li>
-                </ul>
-              </div> --}}
-              <!-- END sidebar-box -->
-
-              <div class="sidebar-box">
-                <h3 class="heading">Tags</h3>
-                <ul class="tags">
-                  @foreach($tags as $tag)
-                    <li><a href="#">{{ $tag->name }}</a></li>
-                  @endforeach
-                </ul>
               </div>
-            </div>
-          <!-- END sidebar -->
+            <!-- END sidebar -->
 
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-    {{-- <section class="py-5">
+      {{-- <section class="py-5">
       <div class="container">
         <div class="row">
           <div class="col-md-12">
@@ -285,10 +174,10 @@
       </div>
 
 
-    </section> --}}
-    <!-- END section -->
+      </section> --}}
+      <!-- END section -->
   
-    @include('partials._wordify.footer')
+      @include('partials._wordify.footer')
       <!-- END footer -->
 
     </div>
